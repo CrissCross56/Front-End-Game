5493dd8a2b2a7e928f4da8fffe73ff406a934a3d
CrissCross56 <grant.terdoslavich@gmail.com>
2023-03-24 17:28:54 -0400

    correct and incorrect input arrays are working, bug where multiple keys being pressed causes arrays to over fill is still occuring

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const testStr = 'racecar';
 //first nested array contains letters that are in the answer
 //second nested array contains letters that are not in the answer
 const inputArr = [[],[]];
-
+const errArr = [];
+const corrArr = [];
 
 function init(){
 
@@ -14,7 +15,7 @@ function init(){
 
     //have a test string
     
-
+    
 
 
     let canvas = document.querySelector('canvas');
@@ -23,15 +24,54 @@ function init(){
 
 
 
+    //once we checked a letter make sure we arent duplicating
+    function checkDuplicates(indChar, arr){
+        let contains = false;
+        console.log(indChar)
+        //loop thru all
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] === indChar){
+                contains = true;
+            }
+            else{
+                contains = false;
+            }
+        }
+        return contains;
+    }
 
     function checkLetter(e){
-
-        if(testStr.contains(e.key)){
-            console.log(`${e.key} is a part of the word`)
-
+        //push the key being pressed into an array so we can make sure 
+        //that we are only allowed to press one key at a time
+        const ans = [];
+        ans.push(e.key);
+        //make sure that theres only one thing inside ans
+        if(ans.length > 1){
+            ans.pop();
         }
 
-        console.log(inputArr);
+        if(testStr.includes(ans[0])){
+            //use the corrArr
+            if(!(checkDuplicates(ans[0],corrArr))){
+                console.log(`${e.key} is a part of the word`);
+                corrArr.push(ans.pop());
+            }
+            else{
+                
+                console.log(`you already correctly guessed the letter ${e.key}`);
+            }
+        }
+        else{
+            if(checkDuplicates(ans[0],errArr)){
+                console.log(`you already incorrectly guessed the letter ${e.key}`);
+            }
+            else{
+                console.log(`you guessed wrong`);
+                errArr.push(ans.pop());
+            }
+        }
+
+        console.log(corrArr, errArr);
     }
 
     function clearInput(){
