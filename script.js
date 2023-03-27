bf0ae9c770686c6ca8b205a8313e1a69128dd007
CrissCross56 <grant.terdoslavich@gmail.com>
2023-03-27 13:13:46 -0400

    logic to fill hiddenStr with the correct characters and win condition is set

--- script.js
+++ script.js
@@ -1,84 +1,93 @@
 window.onload = init;
 
-const testStr = 'racecar';
-//first nested array contains letters that are in the answer
-//second nested array contains letters that are not in the answer
-const inputArr = [[],[]];
-const errArr = [];
-const corrArr = [];
+
 
 function init(){
+
+    let chances = 6;
+    const testStr = 'racecar';
+    const hiddenString = [];
+    const errArr = [];
+    const corrArr = [];
+    const ansIdxs = [];
+
 
     let input = document.querySelector('#input');
     input.addEventListener('keydown',checkLetter);
     input.addEventListener('keyup', clearInput);
 
-    //have a test string
-    
-    
-
-
+    //initialize the hiddenString
+    setupBoard(testStr);
+   
+    //se we can setup and display the hangman and his noose later
     let canvas = document.querySelector('canvas');
     let ctx = canvas.getContext('2d');
 
-
-
-
     //once we checked a letter make sure we arent duplicating
-    function checkDuplicates(indChar, arr){
-        let contains = false;
-        console.log(indChar)
-        //loop thru all
-        for(let i = 0; i < arr.length; i++){
-            if(arr[i] === indChar){
-                contains = true;
-            }
-            else{
-                contains = false;
-            }
-        }
-        return contains;
+    function checkDuplicates(event, arr){
+        return arr.includes(event.key);
     }
 
+    //to be called each time a key is pressed inside the input form
     function checkLetter(e){
         //push the key being pressed into an array so we can make sure 
         //that we are only allowed to press one key at a time
-        const ans = [];
-        ans.push(e.key);
-        //make sure that theres only one thing inside ans
-        if(ans.length > 1){
-            ans.pop();
-        }
+        
 
-        if(testStr.includes(ans[0])){
+        if(testStr.includes(e.key)){
             //use the corrArr
-            if(!(checkDuplicates(ans[0],corrArr))){
-                console.log(`${e.key} is a part of the word`);
-                corrArr.push(ans.pop());
+            if(checkDuplicates(e,corrArr)){
+                console.log(`you already correctly guessed the letter ${e.key}`);
             }
             else{
-                
-                console.log(`you already correctly guessed the letter ${e.key}`);
+                console.log(`${e.key} is a part of the word`);
+                corrArr.push(e.key);
+                //call code for updating the hiddenString in here
+                fillLetters(e.key);
             }
         }
         else{
-            if(checkDuplicates(ans[0],errArr)){
+            if(checkDuplicates(e,errArr)){
                 console.log(`you already incorrectly guessed the letter ${e.key}`);
             }
             else{
                 console.log(`you guessed wrong`);
-                errArr.push(ans.pop());
+                errArr.push(e.key);
+            
             }
         }
 
         console.log(corrArr, errArr);
     }
 
+    //we wanna clear the input field
     function clearInput(){
         input.value = ' ';
     }
 
     
+    //we wanna fill out the array that has _ chars
+    function fillLetters(letter){
+        for(let i = 0; i < testStr.length; i ++){
+            if(testStr[i] === letter){
+                hiddenString[i] = letter;
+            }
+        }
+        //if no more _'s then the player has at this point guessed all the letters
+        //check to see if there are no more 'empty' spaces in the hiddenStr
+        if(!(hiddenString.includes('_'))){
+            alert("You win!")
+        }
+        console.log(hiddenString);
+    }
+
+    
+    function setupBoard(testStr){
+        for(let i = 0; i < testStr.length; i ++){
+            hiddenString.push('_');
+        }
+        console.log(hiddenString)
+    }
 
 };
 
@@ -93,5 +102,6 @@ function drawGallows(ctx){
 }
 
 function hangMan(ctx,partIdx){
-
+    ctx.save();
+    ctx.restore();
 }
