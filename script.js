f8283fe715aeaf255fcca011ea33a2acfcfdc74b
CrissCross56 <grant.terdoslavich@gmail.com>
2023-03-27 16:25:57 -0400

    drawing the gallows and parts of the hangman, logic for the rules works

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ function init(){
     const hiddenString = [];
     const errArr = [];
     const corrArr = [];
-    const ansIdxs = [];
+    
 
-
+    let reset = document.querySelector('#reset');
     let input = document.querySelector('#input');
     input.addEventListener('keydown',checkLetter);
     input.addEventListener('keyup', clearInput);
-
-    //initialize the hiddenString
-    setupBoard(testStr);
+    reset.addEventListener('click', resetGame);
    
     //se we can setup and display the hangman and his noose later
     let canvas = document.querySelector('canvas');
     let ctx = canvas.getContext('2d');
+
+    //initialize the hiddenString and other game variables
+    setupBoard(testStr);
 
     //once we checked a letter make sure we arent duplicating
     function checkDuplicates(event, arr){
@@ -30,15 +31,12 @@ function init(){
 
     //to be called each time a key is pressed inside the input form
     function checkLetter(e){
-        //push the key being pressed into an array so we can make sure 
-        //that we are only allowed to press one key at a time
-        
-
+        //if our input is inside the answer string
         if(testStr.includes(e.key)){
-            //use the corrArr
             if(checkDuplicates(e,corrArr)){
                 console.log(`you already correctly guessed the letter ${e.key}`);
             }
+            //if we havent alread guessed the lette, push to the corrArr
             else{
                 console.log(`${e.key} is a part of the word`);
                 corrArr.push(e.key);
@@ -53,7 +51,7 @@ function init(){
             else{
                 console.log(`you guessed wrong`);
                 errArr.push(e.key);
-            
+                removeChance();
             }
         }
 
@@ -81,12 +79,46 @@ function init(){
         console.log(hiddenString);
     }
 
-    
+    //player chose wrong, and they lose a chance
+    function removeChance(){
+        chances--;
+        if(chances <= 0){
+            console.log(`You lose! The word was ${testStr}`)
+        }
+        else{
+            hangMan(ctx,chances);
+            console.log(`You have ${chances} left`)
+        }
+    }
+    //set up the board for whatever size of string we are using for the answer
     function setupBoard(testStr){
+        //initialize everything else in here
+        
+        chances = 6;
+        hiddenString.length = 0;
+        errArr.length = 0;
+        corrArr.length = 0;
+
+
         for(let i = 0; i < testStr.length; i ++){
             hiddenString.push('_');
         }
         console.log(hiddenString)
+
+
+        // "wipe" the canvas clean
+        ctx.restore();
+
+        //and then draw again
+
+        //the gallows
+        drawGallows(ctx);
+
+    }
+
+    //reset the game once you lose or if the player gives up
+    function resetGame(){
+        setupBoard(testStr);
     }
 
 };
@@ -97,11 +129,87 @@ function init(){
 function drawGallows(ctx){
     ctx.save();
     ctx.strokeStyle = "black";
+
+    //first vertical line
     ctx.beginPath();
+            //x1,y1
+    ctx.moveTo(300,50);
+            //x2,y2
+    ctx.lineTo(300,400);
+    //
+    ctx.closePath();
+    ctx.stroke();
+
+    //second line
+    ctx.beginPath();
+    ctx.moveTo(300, 50)
+    ctx.lineTo(450,50);
+    ctx.closePath();
+    ctx.stroke();
+
+    //third line
+    ctx.beginPath();
+    ctx.moveTo(450,50);
+    ctx.lineTo(450,100);
+    ctx.closePath();
+    ctx.stroke();
+
     ctx.restore();
 }
 
 function hangMan(ctx,partIdx){
     ctx.save();
+    ctx.strokeStyle = 'black';
+        //check to see what case we're in
+        switch (partIdx) {
+            case 5:
+                //draw the head
+                ctx.beginPath();
+                //    x,y,radius,startAngle,endAngle, counterClockwise?
+                ctx.arc(450,125,25,0, 2 * Math.PI);
+                ctx.closePath();
+                ctx.stroke();
+                break;
+            case 4:
+                //draw the body
+                ctx.beginPath();
+                ctx.moveTo(450,150);
+                ctx.lineTo(450,225);
+                ctx.closePath();
+                ctx.stroke();
+                break;
+            case 3:
+                //draw the right arm
+                ctx.beginPath();
+                ctx.moveTo(450,160);
+                ctx.lineTo(500,170);
+                ctx.closePath();
+                ctx.stroke();
+                break;
+            case 2:
+                //draw the left arm
+                ctx.beginPath();
+                ctx.moveTo(450,160);
+                ctx.lineTo(400,170);
+                ctx.closePath();
+                ctx.stroke();
+                break;
+            case 1:
+                //draw the right leg
+                ctx.beginPath();
+                ctx.moveTo(450);
+                ctx.lineTo();
+                ctx.closePath();
+                ctx.stroke();
+                break;
+            case 0:
+                //draw the left leg
+                break;
+            default:
+                console.log('something is going horribly wrong here')
+                break;
+        }
+
+
     ctx.restore();
 }
